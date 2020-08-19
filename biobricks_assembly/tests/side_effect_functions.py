3069a62dd24cfdcc0fbebcc327dd84f1b4bc03d4
Gabrielle Johnston <41130455+GabrielleJohnston@users.noreply.github.com>
2020-08-19 14:09:28 +0100

    Update side_effect_functions.py

--- biobricks_assembly/tests/side_effect_functions.py
+++ biobricks_assembly/tests/side_effect_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import sys 
+import sys
+import os
 parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, '/biobricks10'))
 sys.path.append(parentdir)
 import bbinput

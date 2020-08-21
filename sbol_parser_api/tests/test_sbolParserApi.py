6ce330e5661bec67ed1bc404ea3f6419addb586c
Olivia Gallupová <olivia.gallup@gmail.com>
2020-08-21 17:28:12 +0100

    Updates to unit test for list making

--- sbol_parser_api/tests/test_sbolParserApi.py
+++ sbol_parser_api/tests/test_sbolParserApi.py
@@ -19,6 +19,13 @@ class TestSbolParserApi(TestCase):
         self.doc = sbol2.Document(filepath)
         self.parser = ParserSBOL(sbol2.Document(filepath))
 
+    def generate_dummy_components(self, num_comps=5):
+        names = np.random.rand(num_comps)
+        list_of_constructs = [sbol2.componentdefinition.ComponentDefinition(str(name)[0:5]) for name in names]
+        for construct in list_of_constructs:
+            self.doc.addComponentDefinition(construct)
+        return list_of_constructs
+
     def test_generateCsv_for_DNABot(self):
         raise NotImplementedError("Not yet implemented")
 
@@ -29,24 +36,32 @@ class TestSbolParserApi(TestCase):
         for cd in self.parser.getRootComponenentDefinitions():
             self.assertEquals(cd.displayId, 'Dummy')
 
-    def test_getSortedListOfParts(self):
-        names = np.random.rand(5)
-        listOfConstructs = [sbol2.componentdefinition.ComponentDefinition(str(name)[0:5]) for name in names]
-        sorted_parts = self.parser.getSortedListOfParts(listOfConstructs)
+    def test_getListOfParts(self):
+        allConstructs = self.generate_dummy_components()
+        construct = allConstructs[0]
+        print('construct', construct)
+        component = construct.components
+        print('component', component)
+        print('type(component)', type(component))
+        defin = component.definition
+        # print('\n\nallConstructs[0].components.definition', allConstructs[0].components.definition)
 
+        list_of_parts = self.parser.getListOfParts(allConstructs)
+        print('\n\nlist_of_parts', list_of_parts)
+        assert type(list_of_parts[0]) == sbol2.componentdefinition.ComponentDefinition
+
+    def test_getSortedListOfParts(self):
+        listOfConstructs = self.generate_dummy_components()
+        sorted_parts = self.parser.getSortedListOfParts(listOfConstructs)
         assert sorted_parts[0].displayId < sorted_parts[1].displayId, f'Sorting of component definitions not working, {sorted_parts[0].displayId} is not smaller than {sorted_parts[1]}'
 
     def test_getDictOfComponents(self):
-        names = np.random.rand(5)
-        listOfConstructs = [sbol2.componentdefinition.ComponentDefinition(str(name)[0:5]) for name in names]
-        uris = []
-        for construct in listOfConstructs:
-            self.doc.addComponentDefinition(construct)
-            uris.append(construct.uris)
-        constructs_dict = self.parser.getDictOfComponents(listOfConstructs)  
+        listOfConstructs = self.generate_dummy_components()
+        raise NotImplementedError("In the parser getDictOfComponents requires the input components to have sequence constraints")
 
+        constructs_dict = self.parser.getDictOfComponents(listOfConstructs)  
         for key in constructs_dict.keys():
             assert type(key) == str, "Keys in dictionary of components not 'str' type displayId"
             assert constructs_dict[key] == list, "Values in dictionary of components not list"
 
-    # def test_
+    

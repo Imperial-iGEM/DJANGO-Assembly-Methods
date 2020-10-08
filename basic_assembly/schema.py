d0fff635082ffaea0f11bfa28ff805367064911d
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-10-09 01:50:21 +0800

    Remove unused files

--- basic_assembly/schema.py
+++
@@ -1,5 +0,0 @@
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello_basic = graphene.String(default_value="Hi From Basic Assembly")
--- biobricks_assembly/schema.py
+++
@@ -1,5 +0,0 @@
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello_biobricks = graphene.String(default_value="Hi From Bio Bricks Assembly")

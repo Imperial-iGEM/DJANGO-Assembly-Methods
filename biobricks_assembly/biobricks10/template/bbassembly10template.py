224300314dbae1f52a48f67579b6880c647faa4f
Gabrielle Johnston <41130455+GabrielleJohnston@users.noreply.github.com>
2020-10-24 17:10:24 +0100

    Fix template biobricks

--- biobricks_assembly/biobricks10/template/bbassembly10template.py
+++ biobricks_assembly/biobricks10/template/bbassembly10template.py
@@ -23,17 +23,19 @@ metadata = {'apiLevel': '2.2',
     pipette is not using
     transfer_t4_manually: transfer the t4 ligase and t4 ligase buffer manually
     so that you don't any on dead volume
+    water_trough: True or False -> makes water in trough and other reagents in
+    extra well_plate (other reagents includes digest storage)
 '''
 
 
 def run(protocol: protocol_api.ProtocolContext):
-    def bbassemble(source_to_digest, digest_to_storage, digest_to_construct,
-                   reagent_to_digest, reagent_to_construct, p10_mount='left',
+    def bbassemble(source_to_digest, digest_to_construct, reagent_to_digest,
+                   reagent_to_construct, reagents_dict, p10_mount='left',
                    p10_type='p10_single',
                    well_plate_type='biorad_96_wellplate_200ul_pcr',
                    tube_rack_type='opentrons_24_tuberack_nest_1.5ml_snapcap',
                    thermocycle=False, use_p300=False,
-                   transfer_t4_manually=False):
+                   transfer_t4_manually=False, water_trough=False):
         # Define constants
         CANDIDATE_TIPRACK_SLOT = '3'
         TIPRACK_TYPE = 'opentrons_96_tiprack_10ul'
@@ -44,22 +46,30 @@ def run(protocol: protocol_api.ProtocolContext):
         DESTINATION_PLATE_TYPE = well_plate_type
         TUBE_RACK_TYPE = tube_rack_type
         TUBE_RACK_POSITION = '4'
-        T4_BUFFER_VOL = 2
-        T4_LIGASE_VOL = 1
 
         # load labware
         source_plate = protocol.load_labware(SOURCE_PLATE_TYPE,
                                              SOURCE_PLATE_POSITION)
-        tube_rack = protocol.load_labware(TUBE_RACK_TYPE, TUBE_RACK_POSITION)
+        if water_trough:
+            reagents_plate = protocol.load_labware(SOURCE_PLATE_TYPE,
+                                                   TUBE_RACK_POSITION)
+            trough = protocol.load_labware('usascientific_12_reservoir_22ml',
+                                           '6')
+            water_well = trough.wells()[0]
+        else:
+            tube_rack = protocol.load_labware(
+                TUBE_RACK_TYPE, TUBE_RACK_POSITION)
         tip_rack = protocol.load_labware(TIPRACK_TYPE, CANDIDATE_TIPRACK_SLOT)
         pipette = protocol.load_instrument(PIPETTE_TYPE, PIPETTE_MOUNT,
                                            tip_racks=[tip_rack])
         pipette.flow_rate.aspirate = 20
         if thermocycle:
             tc_mod = protocol.load_module('Thermocycler Module')
-            dest_plate = tc_mod.load_labware(DESTINATION_PLATE_TYPE)
+            # dest_plate = tc_mod.load_labware(DESTINATION_PLATE_TYPE)
+            digest_plate = tc_mod.load_labware(DESTINATION_PLATE_TYPE)
             tc_mod.open_lid()
         else:
+            digest_plate = protocol.load_labware(DESTINATION_PLATE_TYPE, '1')
             dest_plate = protocol.load_labware(DESTINATION_PLATE_TYPE, '7')
 
         '''
@@ -76,14 +86,21 @@ def run(protocol: protocol_api.ProtocolContext):
                 'p300_single', p300_mount, tip_racks=[p300_tiprack])
         # transferring reagents
         for reagent_well in reagent_to_digest.keys():
-            reagent_plate_well = tube_rack.wells_by_name()[reagent_well]
+            if water_trough:
+                if reagent_well == reagents_dict['water']:
+                    reagent_plate_well = water_well
+                else:
+                    reagent_plate_well = reagents_plate.wells_by_name()[
+                        reagent_well]
+            else:
+                reagent_plate_well = tube_rack.wells_by_name()[reagent_well]
             val = reagent_to_digest[reagent_well]
             vols = [entry[1] for entry in val]
 
             if use_p300 and all(vol >= 30 for vol in vols):
                 p300_pipette.pick_up_tip()
                 for i in range(len(val)):
-                    dest_well = dest_plate.wells_by_name()[val[i][0]]
+                    dest_well = digest_plate.wells_by_name()[val[i][0]]
                     vol = val[i][1]
                     p300_pipette.transfer(vol, reagent_plate_well, dest_well,
                                           new_tip='never')
@@ -91,14 +108,14 @@ def run(protocol: protocol_api.ProtocolContext):
             else:
                 pipette.pick_up_tip()
                 for i in range(len(val)):
-                    dest_well = dest_plate.wells_by_name()[val[i][0]]
+                    dest_well = digest_plate.wells_by_name()[val[i][0]]
                     vol = val[i][1]
                     pipette.transfer(vol, reagent_plate_well, dest_well,
                                      new_tip='never')
                 pipette.drop_tip()
 
         digest_wells = []
-        digest_wells = [dest_plate.wells_by_name()[key] for key in
+        digest_wells = [digest_plate.wells_by_name()[key] for key in
                         digest_to_construct.keys()]
         # Part transfers to digest tubes
         for source_well in source_to_digest.keys():
@@ -107,26 +124,24 @@ def run(protocol: protocol_api.ProtocolContext):
             digest_wells = []
             # vols = []
             for i in range(len(val)):
-                digest_wells.append(dest_plate.wells_by_name()[val[i][0]])
+                digest_wells.append(digest_plate.wells_by_name()[val[i][0]])
                 # vols.append(val[i][1])
             vol = val[0][1]
-
-            pipette.pick_up_tip()
-            for digest_well in digest_wells:
-                pipette.move_to(source_plate_well.bottom())
-                protocol.max_speeds['Z'] = 10
-                pipette.aspirate(vol, source_plate_well.bottom())
-                pipette.move_to(digest_well.top())
-                protocol.max_speeds['Z'] = None
-                pipette.dispense(vol, digest_well)
-                pipette.touch_tip(digest_well)
-                pipette.blow_out()
-            pipette.drop_tip()
-
-        for digest_well in digest_wells:
-            pipette.pick_up_tip()
-            pipette.mix(5, 10, digest_well)
-            pipette.drop_tip()
+            if vol < 10:
+                pipette.pick_up_tip()
+                for digest_well in digest_wells:
+                    pipette.move_to(source_plate_well.bottom())
+                    protocol.max_speeds['Z'] = 10
+                    pipette.aspirate(vol, source_plate_well.bottom())
+                    pipette.move_to(digest_well.top())
+                    protocol.max_speeds['Z'] = None
+                    pipette.dispense(vol, digest_well)
+                    pipette.touch_tip(digest_well)
+                    pipette.blow_out()
+                pipette.drop_tip()
+            else:
+                pipette.transfer(vol, source_plate_well.bottom(), digest_wells,
+                                 touch_tip=True, blow_out=True)
 
         if thermocycle:
             protocol.comment("--------------------------------------------")
@@ -137,65 +152,99 @@ def run(protocol: protocol_api.ProtocolContext):
                         {'temperature': 80, 'hold_time_seconds': 1200}]
             tc_mod.execute_profile(steps=profile1, repetitions=1)
             tc_mod.open_lid()
+            protocol.comment(
+                'Remove the digest plate and move it into decks position 1')
+            digest_plate1 = protocol.load_labware(DESTINATION_PLATE_TYPE, '1')
+            protocol.comment(
+                'Place a empty plate of the same type in the thermocycler.'
+            )
         else:
             protocol.pause()
             protocol.comment(
-                'If your reaction plate does not fit in your benchtop thermocycler, please transfer your digests to a compatible plate'
+                'If your digest plate does not fit in your benchtop thermocycler, please transfer your digests to a compatible plate'
             )
             protocol.comment(
-                'Seal the reaction/compatible plate with adhesive film and remove.')
+                'Seal the digest/compatible plate with adhesive film and remove.')
             protocol.comment(
                 'Thermocycle the plate on a benchtop thermocycler.')
             protocol.comment('Use the following thermocycler settings:')
             protocol.comment(
                 '1 cycle of 37°C for 5 minutes and 80°C for 10 minutes')
             protocol.comment('If using a different plate for your thermocycler, please transfer digests back to their original wells.')
-            protocol.comment('Return the reaction plate to position 7.')
-
+            protocol.comment('Return the digest plate to position 7.')
+        protocol.pause()
+        protocol.comment("You may insert your T4 Ligase into well {0}".format(
+            reagents_dict['T4Ligase']))
+        protocol.comment("You may insert your T4 Buffer into well {0}".format(
+            reagents_dict['T4Ligase10X']))
         '''
             Ligation procedure:
         '''
-
-        for reagent_well in reagent_to_construct.keys():
-            reagent_plate_well = tube_rack.wells_by_name()[reagent_well]
-            val = reagent_to_construct[reagent_well]
-            construct_wells = []
-            # vols = []
-            for i in range(len(val)):
-                construct_wells.append(dest_plate.wells_by_name()[val[i][0]])
-                # vols.append(val[i][1])
-            vol = val[0][1]
-            if vol > 10:
-                pipette.transfer(vol, reagent_plate_well, construct_wells)
-            else:
-                if not transfer_t4_manually:
-                    for construct_well in construct_wells:
-                        pipette.pick_up_tip()
-                        pipette.move_to(reagent_plate_well.bottom())
-                        protocol.max_speeds['Z'] = 10
-                        pipette.aspirate(vol, reagent_plate_well.bottom())
-                        pipette.move_to(construct_well.top())
-                        protocol.max_speeds['Z'] = None
-                        pipette.dispense(vol, construct_well)
-                        pipette.touch_tip(construct_well)
-                        pipette.blow_out()
-                        pipette.drop_tip()
-                elif vol == T4_BUFFER_VOL:
-                    protocol.comment('-------Manual transfer-------')
-                    protocol.comment(
-                        "Transfer 2 uL of the T4 Ligase Buffer into each of the construct wells.")
-                elif vol == T4_LIGASE_VOL:
-                    protocol.comment('-------Manual transfer-------')
-                    protocol.comment(
+        # Transfer water
+        water_well_name = reagents_dict['water']
+        val = reagent_to_construct[water_well_name]
+        if water_trough:
+            reagent_plate_well = water_well
+        else:
+            reagent_plate_well = tube_rack.wells_by_name()[water_well_name]
+        if thermocycle:
+            construct_wells = [digest_plate.wells_by_name()[val[i][0]] for i in
+                               range(len(val))]
+        else:
+            construct_wells = [dest_plate.wells_by_name()[val[i][0]] for i in
+                               range(len(val))]
+        pipette.pick_up_tip()
+        for i in range(len(val)):
+            vol = val[i][1]
+            pipette.transfer(vol, reagent_plate_well, construct_wells[i],
+                             new_tip='never')
+        pipette.drop_tip()
+        if transfer_t4_manually:
+            protocol.pause()
+            protocol.comment(
+                "Transfer 2 uL of the T4 Ligase Buffer into each of the construct wells."
+            )
+            protocol.comment(
                         "Transfer 1 uL of the T4 Ligase into each of the construct wells.")
+        else:
+            T4_list = ['T4Ligase10X', 'T4Ligase']
+            for T4_entry in T4_list:
+                well = reagents_dict[T4_entry]
+                val = reagent_to_construct[well]
+                if water_trough:
+                    reagent_plate_well = reagents_plate.wells_by_name()[well]
+                else:
+                    reagent_plate_well = tube_rack.wells_by_name()[well]
+                for i in range(len(val)):
+                    if thermocycle:
+                        construct_well = digest_plate.wells_by_name()[val[i][0]]
+                    else:
+                        construct_well = dest_plate.wells_by_name()[val[i][0]]
+                    vol = val[i][1]
+                    pipette.pick_up_tip()
+                    pipette.move_to(reagent_plate_well.bottom())
+                    protocol.max_speeds['Z'] = 10
+                    pipette.aspirate(vol, reagent_plate_well.bottom())
+                    pipette.move_to(construct_well.top())
+                    protocol.max_speeds['Z'] = None
+                    pipette.dispense(vol, construct_well)
+                    pipette.touch_tip(construct_well)
+                    pipette.blow_out()
+                    pipette.drop_tip()
 
         for digest_well in digest_to_construct.keys():
-            digest_plate_well = dest_plate.wells_by_name()[digest_well]
+            if thermocycle:
+                digest_plate_well = digest_plate1.wells_by_name()[digest_well]
+            else:
+                digest_plate_well = digest_plate.wells_by_name()[digest_well]
             val = digest_to_construct[digest_well]
             construct_wells = []
             # vols = []
             for i in range(len(val)):
-                construct_wells.append(dest_plate.wells_by_name()[val[i][0]])
+                if thermocycle:
+                    construct_wells.append(digest_plate.wells_by_name()[val[i][0]])
+                else:
+                    construct_wells.append(dest_plate.wells_by_name()[val[i][0]])
                 # vols.append(val[i][1])
             vol = val[0][1]
 
@@ -212,47 +261,10 @@ def run(protocol: protocol_api.ProtocolContext):
                 pipette.blow_out()
             pipette.drop_tip()
 
-        for digest_well in digest_to_storage.keys():
-            digest_plate_well = dest_plate.wells_by_name()[digest_well]
-            val = digest_to_storage[digest_well]
-            storage_wells = []
-            # vols = []
-            for i in range(len(val)):
-                storage_wells.append(tube_rack.wells_by_name()[val[i][0]])
-                # vols.append(val[i][1])
-            vol = val[0][1]
-
-            pipette.pick_up_tip()
-            for storage_well in storage_wells:
-                if vol > 10:
-                    full_vol_no = vol // 10
-                    vols = [10]*full_vol_no
-                    if vol % 10 > 0:
-                        vols.append(vol % 10)
-                    for v in vols:
-                        pipette.move_to(digest_plate_well.bottom())
-                        protocol.max_speeds['Z'] = 10
-                        pipette.aspirate(v, digest_plate_well.bottom())
-                        pipette.move_to(storage_well.top())
-                        protocol.max_speeds['Z'] = None
-                        pipette.dispense(v, storage_well)
-                        pipette.touch_tip(storage_well)
-                        pipette.blow_out()
-                else:
-                    pipette.move_to(digest_plate_well.bottom())
-                    protocol.max_speeds['Z'] = 10
-                    pipette.aspirate(vol, digest_plate_well.bottom())
-                    pipette.move_to(storage_well.top())
-                    protocol.max_speeds['Z'] = None
-                    pipette.dispense(vol, storage_well)
-                    pipette.touch_tip(storage_well)
-                    pipette.blow_out()
-            pipette.drop_tip()
-
-        for construct_well in construct_wells:
+        '''for construct_well in construct_wells:
             pipette.pick_up_tip()
             pipette.mix(5, 10, construct_well)
-            pipette.drop_tip()
+            pipette.drop_tip()'''
 
         if thermocycle:
             protocol.comment("--------------------------------------------")
@@ -279,13 +291,7 @@ def run(protocol: protocol_api.ProtocolContext):
 
         protocol.comment('BioBricks assembly finished')
 
-    bbassemble(source_to_digest, digest_to_storage, digest_to_construct,
-               reagent_to_digest, reagent_to_construct, p10_mount=p10_mount,
-               p10_type=p10_type,
-               well_plate_type=well_plate_type, tube_rack_type=tube_rack_type,
-               thermocycle=thermocycle)
-
-    '''
-        Optionally add use_p300 = use_p300,
-        transfer_t4_manually = transfer_t4_manually to bbassemble call
-    '''
+    bbassemble(source_to_digest, digest_to_construct, reagent_to_digest,
+               reagent_to_construct, reagents_dict, p10_mount=p10_mount,
+               p10_type=p10_type, well_plate_type=well_plate_type,
+               tube_rack_type=tube_rack_type, thermocycle=thermocycle)
